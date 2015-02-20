0fd47d47ff99dab96a7c4e58e584cb9e73f6e3b8
tonymadbrain <mb7222@yandex.ru>
2015-02-20 22:35:10 +0300

    finish ep28

--- cart.rb
+++ cart.rb
@@ -4,8 +4,24 @@ class Cart
 
 	include ItemContainer
 
-	def initialize
+	def initialize(owner)
 		@items = []
+		@owner = owner
+	end
+
+	def save_to_file
+		File.open("#{@owner}_cart.txt", "w") do |f|
+			@items.each { |i| f.puts "#{i.name}:#{i.price}:#{i.weight}" }
+		end
+	end
+
+	def read_from_file
+		return unless File.exists?("#{@owner}_cart.txt")
+		item_fields = File.readlines("#{@owner}_cart.txt")
+		item_fields.map! { |i| i.chomp }
+		item_fields.map! { |i| i.split(":") }
+		item_fields.each { |i| @items << Item.new(name: i[0], price: i[1].to_i, weight: i[2].to_i) }
+		@items.uniq!
 	end
 
 end
