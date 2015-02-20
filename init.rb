d03fa30250bf62894422f77e89345529266d25c9
Antony Ryabov <mb7222@yandex.ru>
2015-02-20 15:56:43 +0300

    finish ep27

--- init.rb
+++ init.rb
@@ -5,13 +5,7 @@ require_relative "item"
 require_relative "virtual_item"
 require_relative "real_item"
 
-item1 = VirtualItem.new({price:101, weight:100, name:"Car"})
-item2 = RealItem.new({weight:100, price:101, name:"Car"})
-item3 = RealItem.new({weight:100, price:10, name:"Dishwasher"})
-
-cart = Cart.new
-cart.add_item item1
-cart.add_item item2
-cart.add_item item3
-
-puts cart.items.size
+@items = []
+@items << VirtualItem.new({price:101, weight:100, name:"car"})
+@items << RealItem.new({weight:100, price:101, name:"kettle"})
+@items << RealItem.new({weight:100, price:101, name:"dish"})
