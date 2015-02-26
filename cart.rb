eea5a512794d97317ca3deafec78c9c09e3d9e42
Antony Ryabov <mb7222@yandex.ru>
2015-02-26 10:57:47 +0300

    finish ep30

--- cart.rb
+++ cart.rb
@@ -10,15 +10,16 @@ class Cart
 	end
 
 	def save_to_file
-		File.open("#{@owner}_cart.txt", "w") do |f|
+		File.open("#{ @owner }_cart.txt", "w") do |f|
 			@items.each { |i| f.puts i }
 		end
 	end
 
 	def read_from_file
-		return unless File.exists?("#{@owner}_cart.txt")
-		File.readlines("#{@owner}_cart.txt").each { |i| @items << i.to_real_item }
+		File.readlines("#{ @owner }_cart.txt").each { |i| @items << i.to_real_item }
 		@items.uniq!
+	rescue Errno::ENOENT
+		File.open("#{ @owner }_cart.txt", "w") {}
+		puts "file #{ @owner }_cart.txt created"
 	end
-
 end
