885586a584ead3495424a6389837083beac5c053
Antony Ryabov <mb7222@yandex.ru>
2015-03-02 17:04:24 +0300

    finished ep33

--- init.rb
+++ init.rb
@@ -11,3 +11,10 @@ require_relative "order"
 @items << AntiqueItem.new({price:101, weight:100, name:"car"})
 @items << RealItem.new({weight:100, price:101, name:"kettle"})
 @items << RealItem.new({weight:100, price:101, name:"dish"})
+
+cart = Cart.new("roman")
+cart.add_item RealItem.new({weight:100, price:101, name:"car"})
+cart.add_item RealItem.new({weight:100, price:150, name:"car"})
+cart.add_item RealItem.new({weight:100, price:120, name:"kettle"})
+p cart.all_cars
+p cart.all_kettles
