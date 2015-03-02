59ce27f81880cb10ac314db4bdfce97004697857
Antony Ryabov <mb7222@yandex.ru>
2015-03-02 17:53:36 +0300

    finished ep34

--- init.rb
+++ init.rb
@@ -16,5 +16,8 @@ cart = Cart.new("roman")
 cart.add_item RealItem.new({weight:100, price:101, name:"car"})
 cart.add_item RealItem.new({weight:100, price:150, name:"car"})
 cart.add_item RealItem.new({weight:100, price:120, name:"kettle"})
-p cart.all_cars
-p cart.all_kettles
+
+puts @items[0].kind_of?(Item) 		# является ли конкретный объект, на котором вызван этот метод, экземпляром конкретного класса
+puts @items[0].class == AntiqueItem # выдает класс объекта
+puts @items[0].respond_to?(:info) 	# существует ли конкретный публичный метод
+puts @items[0].send(:tax)			# вызывает конкретный метод на объекте, даже приватный, можно использовать для передачи метода в переменной
