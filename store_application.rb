5b1d3c19d64cc83693a2c9f90cf4404e6c0fbeea
Antony Ryabov <mb7222@yandex.ru>
2015-03-10 10:31:33 +0300

    finished ep36

---
+++ store_application.rb
@@ -0,0 +1,20 @@
+class StoreApplication
+
+	class << self
+		def new
+			unless @instance
+				puts "Loading files ..."
+				require_relative "string"
+				require_relative "item_container"
+				require_relative "item"
+				require_relative "virtual_item"
+				require_relative "real_item"
+				require_relative "antique_item"
+				require_relative "cart"
+				require_relative "order"
+			end
+			@instance ||= self
+		end
+	end
+
+end
