16c3012e2deb719d71fd9835a001f32424e77b4b
Antony Ryabov <mb7222@yandex.ru>
2015-03-10 13:50:41 +0300

    finished ep38

---
+++ duck.rb
@@ -0,0 +1,29 @@
+class Duck
+
+	@@greeting = "Hello"
+
+	def initialize(greeting)
+		@greeting = greeting
+	end
+
+	def quack
+		puts "#{ @@greeting } #{@greeting}"
+	end
+
+	class << self
+		
+		attr_accessor :greeting
+
+		def quack
+			puts "#{ @@greeting } #{@greeting}"
+		end
+	
+	end
+
+end
+
+duck1 = Duck.new("quack quack")
+duck1.quack
+
+Duck.greeting = "Class quack"
+Duck.quack
