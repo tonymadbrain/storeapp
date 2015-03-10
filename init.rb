b7a49060a9efba9672a181379c441462409f3c33
Antony Ryabov <mb7222@yandex.ru>
2015-03-10 09:44:25 +0300

    no {} for hashes when send arguments, finish ep35

--- init.rb
+++ init.rb
@@ -8,14 +8,14 @@ require_relative "cart"
 require_relative "order"
 
 @items = []
-@items << AntiqueItem.new({price:101, weight:100, name:"car"})
-@items << RealItem.new({weight:100, price:101, name:"kettle"})
-@items << RealItem.new({weight:100, price:101, name:"dish"})
+@items << AntiqueItem.new("car", price:101, weight:100)
+@items << RealItem.new(weight:100, price:101, name:"kettle")
+@items << RealItem.new(weight:100, price:101, name:"dish")
 
 cart = Cart.new("roman")
-cart.add_item RealItem.new({weight:100, price:101, name:"car"})
-cart.add_item RealItem.new({weight:100, price:150, name:"car"})
-cart.add_item RealItem.new({weight:100, price:120, name:"kettle"})
+cart.add_item RealItem.new(weight:100, price:101, name:"car")
+cart.add_item RealItem.new(weight:100, price:150, name:"car")
+cart.add_item RealItem.new(weight:100, price:120, name:"kettle")
 
 puts @items[0].kind_of?(Item) 		# является ли конкретный объект, на котором вызван этот метод, экземпляром конкретного класса
 puts @items[0].class == AntiqueItem # выдает класс объекта
--- item.rb
+++ item.rb
@@ -10,9 +10,9 @@ class Item
 		end
 	end
 
-	def initialize(options={})
+	def initialize(name, options={})
 		@real_price  = options[:price]
-		@name   = options[:name]
+		@name   = name
 	end
 
 	attr_reader :real_price, :name
