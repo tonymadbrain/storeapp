4a3392f4ea379017de034eba6bb5430d34e933a4
Antony Ryabov <mb7222@yandex.ru>
2015-04-05 14:37:40 +0300

    finished ep46

--- spec/cart_spec.rb
+++ spec/cart_spec.rb
@@ -7,11 +7,21 @@ require_relative '../app/cart'
 
 describe Cart do
   
-  it "adds items into the cart" do
-    cart = Cart.new("anton")
-    item1 = Item.new("twix", price: 100)
-    item2 = Item.new("mars", price: 200)
-    cart.add_items(item1, item2)
-    expect(cart.items).to include(item1, item2)
+  describe "managing items" do
+    it "adds items into the cart" do
+      cart = Cart.new("anton")
+      item1 = Item.new("twix", price: 100)
+      item2 = Item.new("mars", price: 200)
+      cart.add_items(item1, item2)
+      expect(cart.items).to include(item1, item2)
+    end
+
+    it "remove items from itself"  
   end
+
+  it "counts items in itself" do
+    skip
+  end
+  it "place order using all the items that were added into the cart"
+  it "clears itself off the items after an order is placed"
 end
